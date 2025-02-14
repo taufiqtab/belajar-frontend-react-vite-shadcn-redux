c3af2db2aec10c3d311c92c416f4ece1183d45c7
Taufiq Abdullah <taufiqtab77@gmail.com>
2025-02-14 22:51:08 +0700

    ✨feature: react redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
-import { Input } from './components/ui/input'
+// import { Input } from './components/ui/input'
+import { useAppSelector, useAppDispatch } from './app/hooks'
+import { decrement, increment } from './features/counter/counterSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [nama, setNama] = useState("")
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
 
-  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
-    setNama(event.target.value);
-  }
+  // const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+  //   setNama(event.target.value);
+  // }
 
   return (
     <>
@@ -18,14 +20,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <p>Angka sekarang adalah {count}</p>
-        <Button onClick={() => setCount((count) => count - 1)}>-</Button>
-        <Button onClick={() => setCount((count) => count + 1)}>+</Button>
+        <Button onClick={() => dispatch(decrement())}>-</Button>
+        <Button onClick={() => dispatch(increment())}>+</Button>
         <p>
-          Belajar Frontend bersama {nama}
+          Belajar Frontend bersama
         </p>
       </div>
 
-      <Input placeholder='Nama' onChange={handleNameChange} />
+      {/* <Input placeholder='Nama' onChange={handleNameChange} /> */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
